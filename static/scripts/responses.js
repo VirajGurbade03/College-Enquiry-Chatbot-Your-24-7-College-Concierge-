59197e205312166d83c9f82757e287381d3ea0c8
VIRAJ VIJAY GURBADE <115770356+VirajGurbade03@users.noreply.github.com>
2024-04-06 13:59:42 +0530

    Add files via upload

---
+++ static/scripts/responses.js
@@ -0,0 +1,92 @@
+function getBotResponse(input) {
+
+    if (input == "hi") {
+        return "Hello their!"
+    } else if (input == "hii") {
+        return "Hello their!"
+    } else if (input == "hello") {
+        return "Hello their!"
+    }
+    else if (input == "collage location") {
+        return " This is the link of the college address copy this location :- https://goo.gl/maps /FBQxsqH3CEyUY9D59 "
+    } else if (input == "where is collage situated") {
+        return " This is the link of the college address copy this location :- https://goo.gl/maps /FBQxsqH3CEyUY9D59 "
+    } else if (input == "where is collage") {
+        return " This is the link of the college address copy this location :- https://goo.gl/maps /FBQxsqH3CEyUY9D59 "
+    } else if (input == "collage ka location kay hai") {
+        return " This is the link of the college address copy this location :- https://goo.gl/maps /FBQxsqH3CEyUY9D59 "
+    }
+    else if (input == "what is the mode of examination") {
+        return "Class test (CT) and one unversity exam (2 Sem in a year)"
+    } else if (input == "how are exam conducted") {
+        return "Class test (CT) and one unversity exam (2 Sem in a year)"
+    } else if (input == "is exam is offline mode") {
+        return "Class test (CT) and one unversity exam (2 Sem in a year)"
+    } else if (input == "how many exam are held in one semester") {
+        return "Class test (CT) and one unversity exam (2 Sem in a year)"
+    }
+    else if (input == "what are the event conducted here") {
+        return "Mega event-IGNITRA in month of jan, along with all monthly Events"
+    } else if (input == "which event are celebrated in the collage") {
+        return "Mega event-IGNITRA in month of jan, along with all monthly Events"
+    } else if (input == "is any annual event organised in the collage") {
+        return "Mega event-IGNITRA in month of jan, along with all monthly Events"
+    } else if (input == "function held in this collage") {
+        return "Mega event-IGNITRA in month of jan, along with all monthly Events"
+    }
+    else if (input == "which lab are avaible in this collage") {
+        return "You will enjoy the benefit of: DATA SCIENCE LAB, E-YANTRA LAB, MATHS LAB, INTEL LAB, CHEMISTRY LAB, CC LAB, PHYSIC LAB, ELECRICAL LAB, COOMMUNICATION LAB"
+    } else if (input == "which lab are available for students") {
+        return "you will enjoy the benefit of: DATA SCIENCE LAB, E-YANTRA LAB, MATHS LAB, INTEL LAB, CHEMISTRY LAB, CC LAB, PHYSIC LAB, ELECRTCAL LAB, COOMMUNICATION LAB"
+    } else if (input == "what are the lab facilities students here can enjoy") {
+        return "you will enjoy the benefit of: DATA SCIENCE LAB, E-YANTRA LAB, MATHS LAB, INTEL LAB, CHEMISTRY LAB, CC LAB, PHYSIC LAB, ELECRTCAL LAB, COOMMUNTCATION LAB"
+    } else if (input == "do collage have labs for each branches individually") {
+        return "you will enjoy the benefit of: DATA SCIENCE LAB, E-YANTRA LAB, MATHS LAB, INTEL LAB, CHEMISTRY LAB, CC LAB, PHYSIS LAB, ELECRTCAL LAB, COOMMUNTCATION LAB"
+    }
+    else if (input == "what is the mode of the collage,dummy and regular") {
+        return "Regular collage (9am-5am),75% mandatory attendance..!"
+    } else if (input == "can we get degree by attending collage in online mode") {
+        return "Regular collage (9am-5am),75% mandatory attendance..!"
+    } else if (input == "is collage scheduled on regular basis") {
+        return "Regular college (9am-5am),75% mandatory attendance..!"
+    } else if (input == "is collage conducted in regular basis") {
+        return "Regular collage (9am-5am),75% mandatory attendance..!"
+    }
+    else if (input == "what is the total fees for one year") {
+        return "1 lakhs per year"
+    } else if (input == "how much amount will i have to pay in one year") {
+        return "1 lakhs per year"
+    } else if (input == "what is the fees for first year") {
+        return "1 lakhs per year"
+    } else if (input == "total amount i have to pay in b.tech first year in branch") {
+        return "1 lakhs per year"
+    }
+    else if (input == "what are the criteria to be followed to get admission here") {
+        return "HSC/JEE/CET/NEET/DMIHER EXAM"
+    } else if (input == "how can i get addmission in the collage") {
+        return "HSC/JEE/CET/NEET/DMIHER EXAM"
+    } else if (input == "which exam should i qualify to get admission heare") {
+        return "HSC/JEE/CET/NEET/DMIHER EXAM"
+    } else if (input == "what is the criteria of the collage for addmission") {
+        return "HSC/JEE/CET/NEET/DMIHER EXAM"
+    }
+    else if (input == "what are the courses we can study in this collage") {
+        return "Aritifical Intellgence and Data Science, Aritifical Intellgence and Machine Learning, Computer Science and Designs, Copmuter Science and Medical Engineering "
+    } else if (input == "which branches are avaible hear") {
+        return "Aritifical Intellgence and Data Science, Aritifical Intellgence and Machine Learning, Computer Science and Designs, Copmuter Science and Medical Engineering "
+    } else if (input == "in which discipline will get b.tec degree in") {
+        return "Aritifical Intellgence and Data Science, Aritifical Intellgence and Machine Learning, Computer Science and Designs, Copmuter Science and Medical Engineering "
+    }
+    else if (input == "do collage have itd own library") {
+        return "Library is avaible with free WIFI for students (8am to 10pm)"
+    } else if (input == "can student get library facility") {
+        return "Library is avaible with free WIFI for students (8am to 10pm)"
+    } else if (input == "collage mai library avaiable hai") {
+        return "Library is avaible with free WIFI for students (8am to 10pm)"
+    } else if (input == "collage mai library hai kay") {
+        return "Library is avaible with free WIFI for students (8am to 10pm)"
+    }
+    else {
+        return "Sorry!, I didn't get that. can you say it again?"
+    }
+}
